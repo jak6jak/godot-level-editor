4d775147f4fc3b0fbfc6116601f395d2cbe62105
Jacob Edie <jacobedie1@gmail.com>
2023-12-28 02:40:01 -0500

    remove simple /*

--- src/GizmoTest.cpp
+++ src/GizmoTest.cpp
@@ -1,49 +1,49 @@
+#include "GizmoTest.h"
 #include "godot_cpp/classes/global_constants.hpp"
 #include "godot_cpp/classes/label.hpp"
+#include "godot_cpp/classes/standard_material3d.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/variant/utility_functions.hpp"
-#include "GizmoTest.h"
-#include "godot_cpp/classes/standard_material3d.hpp"
 
-
-GizmoTest::GizmoTest(){
- godot::UtilityFunctions::print("Hello!");
- create_material("main", godot::Color(1,0,0));
- create_handle_material("handles");
+GizmoTest::GizmoTest()
+{
+    godot::UtilityFunctions::print( "Hello!" );
+    create_material( "main", godot::Color( 1, 0, 0 ) );
+    create_handle_material( "handles" );
 }
 
 GizmoTest::~GizmoTest()
 {
 }
 
-godot::String GizmoTest::_get_gizmo_name() const {
+godot::String GizmoTest::_get_gizmo_name() const
+{
     return "TestGizmo";
 }
 
 bool GizmoTest::_has_gizmo( godot::Node3D *for_node_3d ) const
 {
-    
-    return for_node_3d->get_name() == godot::StringName("help");
+
+    return for_node_3d->get_name() == godot::StringName( "help" );
 }
 
 void GizmoTest::_redraw( const godot::Ref<godot::EditorNode3DGizmo> &gizmo )
 {
     gizmo->clear();
 
-    //godot::Node3D* node3d = gizmo->get_node_3d();
+    // godot::Node3D* node3d = gizmo->get_node_3d();
     godot::PackedVector3Array lines = godot::PackedVector3Array();
 
-    lines.push_back(godot::Vector3(0.0,1.0,0.0));
-    lines.push_back(godot::Vector3(0,5,0));
+    lines.push_back( godot::Vector3( 0.0, 1.0, 0.0 ) );
+    lines.push_back( godot::Vector3( 0, 5, 0 ) );
 
     godot::PackedVector3Array handles = godot::PackedVector3Array();
 
-    handles.push_back(godot::Vector3(0.0,1.0,0.0));
-    handles.push_back(godot::Vector3(0,5,0));
+    handles.push_back( godot::Vector3( 0.0, 1.0, 0.0 ) );
+    handles.push_back( godot::Vector3( 0, 5, 0 ) );
 
-
-    gizmo->add_lines(lines,get_material("main", gizmo),false);
-    gizmo->add_handles(handles,get_material("handles", gizmo), godot::PackedInt32Array());
+    gizmo->add_lines( lines, get_material( "main", gizmo ), false );
+    gizmo->add_handles( handles, get_material( "handles", gizmo ), godot::PackedInt32Array() );
 }
 
 /*
@@ -80,7 +80,8 @@ int32_t GizmoTest::_subgizmos_intersect_ray( godot::Camera3D *camera,
 }
 
 godot::PackedInt32Array GizmoTest::_subgizmos_intersect_frustum( godot::Camera3D *camera,
-                                                   const godot::TypedArray<godot::Plane> &frustum ) const 
+                                                   const godot::TypedArray<godot::Plane> &frustum )
+const
 {
     godot::PackedInt32Array arr;
     return arr;
@@ -95,9 +96,9 @@ godot::Transform3D GizmoTest::_get_subgizmo_transform(int32_t id ) const{
     godot::Transform3D a;
     return a;
 }
-/*
-void GizmoTest::_commit_subgizmos( const godot::PackedInt32Array &ids, const godot::TypedArray<godot::Transform3D> &restores,
-                            bool cancel )
+
+void GizmoTest::_commit_subgizmos( const godot::PackedInt32Array &ids, const
+godot::TypedArray<godot::Transform3D> &restores, bool cancel )
 {
 
 }
